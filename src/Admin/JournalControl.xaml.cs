f1b60b71b3bbbca63c02bc595431736c928d74b4
Lev <levmamatov@gmail.com>
2026-08-25 17:25:02 +0700

    Add files via upload

--- src/Admin/JournalControl.xaml.cs
+++ src/Admin/JournalControl.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
--- src/Admin/MainWindow.xaml.cs
+++ src/Admin/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 using System.Data.SqlClient;
 using System.Net.Http;
 using System.Net.Http.Json;
